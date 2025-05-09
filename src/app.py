ab8cc585080482804fdbb317ddc79516443ec852
Guy Katz <guykat@my.hit.ac.il>
2025-05-09 10:47:43 +0300

    fix app.py #2

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
-def add(a,b):
-	return a+b
+def add(a, b):
+    return a + b
 
-def multiply(a,b):
-	return a * b
+
+def multiply(a, b):
+    return a * b
+
 
 if __name__ == "__main__":
-	print(f"4+3 = {add(4,3)}")
-	print(f"7 * 5 = {multiply(7,5)}")
+    print(f"4 + 3 = {add(4, 3)}")
+    print(f"7 * 5 = {multiply(7, 5)}")
 
