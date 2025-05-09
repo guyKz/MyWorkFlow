9d1be89ecb62922091eb3338a23157ac4b0be9be
Guy Katz <guykat@my.hit.ac.il>
2025-05-09 10:51:22 +0300

    fix app.py #2

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
+
 def add(a, b):
+    """Add two numbers and return the result."""
     return a + b
 
 
 def multiply(a, b):
+    """Multiply two numbers and return the result."""
     return a * b
 
 
 if __name__ == "__main__":
     print(f"4 + 3 = {add(4, 3)}")
     print(f"7 * 5 = {multiply(7, 5)}")
-    
